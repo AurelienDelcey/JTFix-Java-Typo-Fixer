027e101031e1d0027558529174dfa572334f1ba2
AurelienDelcey <aurelien.delcey@gmail.com>
2026-04-29 15:51:21 +0200

    test(loader): add coverage for parallel loading

--- src/test/java/loader/LoaderTest.java
+++ src/test/java/loader/LoaderTest.java
@@ -19,6 +19,7 @@ class LoaderTest {
 	
 	private static List<Path> pathList = new ArrayList<>();
 	private static Map<Path, DataContext> mapTest = new HashMap<>();
+	private static Map<Path, DataContext> mapTestParallel = new HashMap<>();
 	private final static Path TEST_ROOT = Path.of("src/test/resources/filesTest");
 	private final static Path EMPTY_10_LINES_PATH = Path.of("src/test/resources/filesTest/emptyLine10.java");
 	private final static Path FIXED_328_CHAR_PATH = Path.of("src/test/resources/filesTest/fixed328Chars.java");
@@ -35,8 +36,14 @@ class LoaderTest {
 		};
 		
 		PipeResult<Map<Path, DataContext>> loadResult = loader.Loader.load(pathList);
+		PipeResult<Map<Path, DataContext>> loadParallelResult = loader.Loader.loadParallel(pathList);
 		
 		mapTest = switch(loadResult) {
+		case Success<Map<Path, DataContext>> s -> s.payload();
+		case Failure<Map<Path, DataContext>> f -> fail();
+		};
+		
+		mapTestParallel = switch(loadParallelResult) {
 		case Success<Map<Path, DataContext>> s -> s.payload();
 		case Failure<Map<Path, DataContext>> f -> fail();
 		};
@@ -81,6 +88,46 @@ class LoaderTest {
 		case Failure<Map<Path, DataContext>> f -> {}
 		};
 	}
+	
+	@Test
+	void empty10LinesFile_WithParallelLoading_shouldProduce10Chars() {
+		assertEquals(10, getCharsLengthParallel(EMPTY_10_LINES_PATH));
+	}
+
+	@Test
+	void empty10LinesFile_WithParallelLoading_shouldProduce10LinesOffset() {
+		assertEquals(10, getOffsetsLengthParallel(EMPTY_10_LINES_PATH));
+	}
+
+	@Test
+	void fixed328CharsFile_WithParallelLoading_shouldProduce328Chars() {
+		assertEquals(328, getCharsLengthParallel(FIXED_328_CHAR_PATH));
+	}
+	
+	@Test
+	void fixed328CharsFile_WithParallelLoading_shouldReturnEmptyOffsets() {
+		assertEquals(0, getOffsetsLengthParallel(FIXED_328_CHAR_PATH));
+	}
+	
+	@Test
+	void emptyFile_WithParallelLoading_shouldReturnEmptyCharsArray() {
+		assertEquals(0, getCharsLengthParallel(EMPTY_FILE_PATH));
+	}
+	
+	@Test
+	void emptyFile_WithParallelLoading_shouldReturnEmptyOffsets() {
+		assertEquals(0, getOffsetsLengthParallel(EMPTY_FILE_PATH));
+	}
+	
+	@Test
+	void load_WithParallelLoading_shouldReturnFailure_whenPathDoesNotExist() {
+		PipeResult<Map<Path, DataContext>> loadResult = loader.Loader.loadParallel(incorrectPathList);
+		
+		switch(loadResult) {
+		case Success<Map<Path, DataContext>> s -> fail("return Failure expected, but return Success.");
+		case Failure<Map<Path, DataContext>> f -> {}
+		};
+	}
 
 	private int getOffsetsLength(Path path) {
 		return mapTest.get(path).linesOffsets().length;
@@ -88,6 +135,14 @@ class LoaderTest {
 
 	private int getCharsLength(Path path) {
 		return mapTest.get(path).fileContent().length;
+	}
+	
+	private int getOffsetsLengthParallel(Path path) {
+		return mapTestParallel.get(path).linesOffsets().length;
+	}
+
+	private int getCharsLengthParallel(Path path) {
+		return mapTestParallel.get(path).fileContent().length;
 	}
 
 }
